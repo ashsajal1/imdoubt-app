02a469ffa979b590479a5d8f8f49931b3ec842bb
Ashsajal <ashsajal1@gmail.com>
2025-02-25 18:36:22 +0600

    feat: fetch doubtl

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,25 @@
 import HeroSection from "@/components/hero-section";
+import { db } from "@/db/drizzle";
+import { doubts } from "@/db/schema";
 
-export default function Home() {
+export default async function Home() {
+  const doubtsList = await db.select().from(doubts).limit(5);
+
+  console.log(doubtsList);
+
+  if (doubtsList.length === 0) {
+    return (
+      <main>
+        <HeroSection />
+      </main>
+    );
+  }
+
   return (
     <main>
-      <HeroSection />
+      {doubtsList.map((doubt) => {
+        return <div key={doubt.id}>{doubt.id}</div>;
+      })}
     </main>
   );
 }
